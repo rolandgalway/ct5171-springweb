5eee94b6436b8af4a276c98fa300d56e63725f73
Roland Vasarhelyi <r.vasarhelyi1@universityofgalway.ie>
2025-11-20 20:54:03 +0100

    Modified hello message

--- src/main/java/org/example/springweb/HelloController.java
+++ src/main/java/org/example/springweb/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Testing EC2 CI/CD";
+        return "Testing EC2 CI/CD with better EC2 instance";
     }
 }
