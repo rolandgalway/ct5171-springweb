d166d6cea15a8510068592a8f264cfac448940d0
Roland Vasarhelyi <r.vasarhelyi1@universityofgalway.ie>
2025-11-20 18:42:38 +0100

    Initial spring boot web server

---
+++ src/main/java/org/example/springweb/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.springweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ src/main/java/org/example/springweb/SpringwebApplication.java
@@ -0,0 +1,13 @@
+package org.example.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebApplication.class, args);
+	}
+
+}
